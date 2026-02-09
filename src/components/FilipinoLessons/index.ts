7f6246622784cf35eebc075ed0d68eddf8c27a9a
davidscales7 <davidscales7@gmail.com>
2026-02-09 22:41:33 +0000

    Deploy to GitHub Pages

--- src/components/FilipinoLessons/index.ts
+++
@@ -1,5 +0,0 @@
-// components/Lessons/index.ts
-export { default as ChapterRoadmap } from "./ChapterRoadmap";
-export { default as ChapterRow } from "./ChapterRow";
-export { default as LessonTrack } from "./LessonTrack";
-export { default as LessonNode } from "./LessonNode";
